e5ecb592bdb9060e39fdaab593d77e7ef4578b31
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-12-06 16:06:27 +0100

    Added html css fonts and images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import gear from "./images/gear.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="ring">
+        <svg width="518" height="518" viewBox="0 0 518 518">
+          <circle strokeWidth="9px" x="0" y="y" cx="259" cy="259" r="254" />
+        </svg>
+      </div>
+
+      <div className="timer">
+        <div className="time">
+          <div className="minutes">
+            <input type="text" value="15" disabled />
+          </div>
+          <div className="colon">:</div>
+          <div className="seconds">
+            <input type="text" value="00" disabled />
+          </div>
+        </div>
+        <button className="start">start</button>
+        <button className="settings">
+          <img src={gear} alt="Settings" />
+        </button>
+      </div>
     </div>
   );
 }
